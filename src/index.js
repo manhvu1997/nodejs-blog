93cdd2afbda8e3798dd5adf00866667065ea9b46
Vu Chu Manh <ManhVC@DESKTOP-QJOKK8N.officehn.local>
2021-11-08 01:11:42 +0700

    Route and Controllers

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const exphbs  = require('express-handlebars');
+const route = require('./routes');
 const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname,'public')));
+// Print data in post method
+app.use(express.urlencoded({
+  extended:true
+}));
+app.use(express.json());
 
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // Template engine
 app.engine('hbs', exphbs({
@@ -17,15 +23,9 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
 console.log(path.join(__dirname, 'resource/views'));
 
+// Routes init
+route(app);
 
-app.get('/trangchu', (req, res) => {
-  res.render('home');
-})
-
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
